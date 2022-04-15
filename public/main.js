b96994192383622801ef590b0e1a8b18bfbbd6a4
RooT_Kim <bdd3452@gmail.com>
2022-04-15 17:10:44 +0900

    Commit Text - main.js

--- public/main.js
+++ public/main.js
@@ -3,12 +3,11 @@ let menu = document.getElementsByClassName("menu");
 let content = document.getElementsByClassName("content");
 for (let i = 0; i < menu.length; i++) {
   menu[i].onclick = function () {
-    let tabId = this.querySelector("div");
+    let tabIndex = this;
     console.log(this.classList);
-    document.getElementsByClassName("menu").array.forEach((element) => {
+    document.getElementsByClassName("content").forEach((element) => {
       element.classList.remove("active");
       console.log(element);
     });
-    this.
   };
 }
